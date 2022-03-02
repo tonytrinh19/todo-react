4798220d7e8a748e31f36ea12919f6cc3470721e
Tony Trinh <tony.q.trinh@gmail.com>
2022-03-02 13:28:10 -0800

    initialized todo react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [number, setNumber] = useState(0);
+
+  const handlePlusButton = (e) => {
+    setNumber((current) => (current = current + 1));
+    console.log("click plu");
+  };
+
+  const handleMinusButton = (e) => {
+    setNumber((current) => (current = current - 1));
+    console.log("click minus");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handlePlusButton}>+</button>
+      <p>{number}</p>
+      <button onClick={handleMinusButton}>-</button>
     </div>
   );
 }
