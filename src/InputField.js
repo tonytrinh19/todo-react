3479671ddaca3314c6cda1cf335c0eab88275a86
tonytrinh19 <tony.q.trinh@gmail.com>
2022-03-02 22:32:52 -0800

    Added fixed tasks

--- src/InputField.js
+++ src/InputField.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
+import TasksList from "./TasksList";
 
 const InputField = () => {
-  const [todo, setTodo] = useState("");
+  const [tasks, setTasks] = useState(["Task 1", "Task 2"]);
 
   const handleAddTask = (e) => {
     e.preventDefault();
@@ -14,16 +15,19 @@ const InputField = () => {
   };
 
   return (
-    <form>
-      <input type="text" placeholder="Task"></input>
-      <input onClick={handleAddTask} type="submit" value="Add Task"></input>
-      <input
-        onClick={handleClearTasks}
-        type="submit"
-        value="Clear Tasks"
-      ></input>
-      <p>Number of tasks left: {}</p>
-    </form>
+    <div>
+      <TasksList tasks={tasks} />
+      <form>
+        <input type="text" placeholder="Task"></input>
+        <input onClick={handleAddTask} type="submit" value="Add Task"></input>
+        <input
+          onClick={handleClearTasks}
+          type="submit"
+          value="Clear Tasks"
+        ></input>
+        <p>Number of tasks left: {}</p>
+      </form>
+    </div>
   );
 };
 
